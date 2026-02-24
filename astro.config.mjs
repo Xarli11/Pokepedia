a5792ab029540d887f1a65dd3021bae8103fb066
Xarli11 <carlos.garcia@example.com>
2026-02-25 00:59:59 +0100

    chore: migrate to official domain pokepedia.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pokepedia-nu.vercel.app',
+  site: 'https://pokepedia.app',
   output: 'server',
   adapter: vercel(),
   integrations: [sitemap()],
